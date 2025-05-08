fcbea6595544cb9e0d33a645661353666b465f01
Kevin Martin <dev.kmartin@gmail.com>
2025-05-08 15:18:50 +0200

    chore: update Angular CLI, Core, and Material to 19.2.10, add Angular Animations, and configure environment variables

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { appRoutes } from './app.routes';
+import {appRoutes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    provideAnimations()
+  ],
+
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  dartsMatcherApiUrl: 'http://localhost:8080',
+  dartsMatcherWebsocketUrl: 'ws://localhost:8080',
+  clientId: 'darts-matcher-dev',
+  clientSecret: 'secret',
+};
