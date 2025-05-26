1e782ce15d60c40b91e923ea6c044af089164dd2
Kevin Martin <dev.kmartin@gmail.com>
2025-05-26 13:08:26 +0200

    refactor: x01 match page fetches match via websocket instead of REST API

--- src/app/modules/x01-match/pages/x01-match/x01-match-page.component.ts
+++ src/app/modules/x01-match/pages/x01-match/x01-match-page.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {EMPTY, Subscription, switchMap} from 'rxjs';
-import {DartsMatcherApiService} from '../../../../api/services/darts-matcher-api.service';
+import {concat, EMPTY, Observable, of, Subscription, switchMap} from 'rxjs';
 import {X01Match} from '../../../../models/x01-match/x01-match';
 import {HttpErrorResponse} from '@angular/common/http';
 import {isApiErrorBody} from '../../../../api/error/api-error-body';
@@ -9,6 +8,7 @@ import {NgIf} from '@angular/common';
 import {MatToolbar} from '@angular/material/toolbar';
 import {X01MatchComponent} from '../../components/x01-match/x01-match.component';
 import {isValidObjectId} from '../../../../shared/utils/object-id.utils';
+import {DartsMatcherWebsocketService} from '../../../../api/services/darts-matcher-websocket.service';
 
 @Component({
   selector: 'app-x01-match-page',
@@ -28,7 +28,7 @@ export class X01MatchPageComponent implements OnInit, OnDestroy {
 
   public match: X01Match | null = null;
 
-  constructor(private route: ActivatedRoute, private apiService: DartsMatcherApiService) {
+  constructor(private route: ActivatedRoute, private websocketService: DartsMatcherWebsocketService) {
   }
 
   ngOnInit(): void {
@@ -37,22 +37,31 @@ export class X01MatchPageComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.getMatchSubscription.unsubscribe();
+    this.websocketService.deactivate();
   }
 
   private getMatch() {
-    this.getMatchSubscription.add(this.route.paramMap.pipe(
-        switchMap(params => {
-          const matchId = params.get('id') ?? '';
-          if (!isValidObjectId(matchId)) {
-            this.handleInvalidMatchId();
-            return EMPTY;
-          }
-
-          return this.apiService.getMatch(matchId);
-        })
+    this.getMatchSubscription.add(this.getMatchIdFromRoute().pipe(
+        switchMap(matchId => concat(
+          this.websocketService.subscribeToX01MatchSingleResponse(matchId),
+          this.websocketService.subscribeToX01MatchBroadcast(matchId)
+        ))
       ).subscribe({
         next: (match: X01Match) => this.handleGetMatchSuccess(match),
         error: (err: HttpErrorResponse) => this.handleGetMatchError(err),
+      })
+    );
+  }
+
+  private getMatchIdFromRoute(): Observable<string> {
+    return this.route.paramMap.pipe(
+      switchMap(params => {
+        const matchId = params.get('id') ?? '';
+        if (!isValidObjectId(matchId)) {
+          this.handleInvalidMatchId();
+          return EMPTY;
+        }
+        return of(matchId);
       })
     );
   }
